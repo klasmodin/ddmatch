a3a81ae39601579347d198ec87af12e637f27d49
Klas Modin <klas.modin@gmail.com>
2017-03-05 13:30:09 +0100

    Added functionality for creating animations.

--- display.py
+++ display.py
@@ -10,6 +10,7 @@ Created by Klas Modin on 2014-11-03.
 
 import numpy as np 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class MatchPlot(object):
 	"""
@@ -61,4 +62,44 @@ class MatchPlot(object):
 		for (k,line) in zip(range(len(self.hor_lines)),self.hor_lines):
 			line.set_xdata(phix[skip::skip,:].T[:,k])
 			line.set_ydata(phiy[skip::skip,:].T[:,k])
+
+
+def create_animation(ddplot, dm, nouts=40, niter=200, epsilon=0.2, \
+					warp_type='inverse', logscale=False,\
+					**kwarg):
+	if logscale:
+		anim_indices = np.flipud(np.logspace(np.log10(niter),0,nouts)).astype('int')
+	else:
+		anim_indices = np.arange(0,niter,int(niter/nouts))
+	anim_slice = np.diff(anim_indices)
+	anim_slice = np.where(anim_slice==0,1,anim_slice)
+	anim_indices = np.cumsum(anim_slice)
+
+	if hasattr(ddplot,'dens'):
+		ddplot.I1 = ddplot.dens.get_array().copy()
+		ddplot.Iwarp = ddplot.I1.copy()
+
+	def update_ddplot(num):
+		if num is not 0:
+			k = num-1
+			nit = anim_slice[k]
+			dm.run(nit, epsilon=epsilon, **kwarg)
+			if hasattr(ddplot,'vert_lines'):
+				if warp_type is 'inverse':
+					ddplot.update_warp(dm.phiinvx, dm.phiinvy)
+				else:
+					ddplot.update_warp(dm.phix, dm.phiy)
+			if hasattr(ddplot,'dens'):
+				if warp_type is 'inverse':
+					dm.image_compose(ddplot.I1, dm.phix, dm.phiy, ddplot.Iwarp)
+				else:
+					dm.image_compose(ddplot.I1, dm.phiinvx, dm.phiinvy, ddplot.Iwarp)
+				ddplot.Iwarp *= dm.J
+				ddplot.update_density(ddplot.Iwarp)
+
+		# return ddplot.vert_lines + ddplot.hor_lines
+		return ddplot.axes.artists
+
+	return animation.FuncAnimation(ddplot.axes.figure, update_ddplot, len(anim_indices),\
+									interval=50, blit=True)
 
